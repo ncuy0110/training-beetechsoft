aeb171e193f6375aa15938a92e7301e027199e8b
ncuy0110 <nguyenchonuy2001@gmail.com>
2023-05-17 08:25:49 +0700

    tao interceptor kiem tra quyen admin

---
+++ mvc-spring-boot/src/main/java/com/beetech/mvcspringboot/security/AdminInterceptor.java
@@ -0,0 +1,29 @@
+package com.beetech.mvcspringboot.security;
+
+import com.beetech.mvcspringboot.constants.RoleEnum;
+import com.beetech.mvcspringboot.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.nio.file.AccessDeniedException;
+
+@Component
+public class AdminInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean isAdmin = user.hasRole(RoleEnum.ADMIN);
+        if (!isAdmin) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("application/json");
+            String errorMessage = "{\"error\": \"Forbidden\"}";
+            response.getWriter().write(errorMessage);
+            return false;
+        }
+        return true;
+    }
+}
