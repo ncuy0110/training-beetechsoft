cc76547a2dd1fbb7b4eaa7de9ef33b4cf397be97
ncuy0110 <nguyenchonuy2001@gmail.com>
2023-05-18 17:19:21 +0700

    update try catch resource

--- java-features/src/main/java/org/example/java9/TryCatchResources.java
+++ java-features/src/main/java/org/example/java9/TryCatchResources.java
@@ -1,14 +1,13 @@
 package org.example.java9;
 
 import java.io.BufferedReader;
-import java.io.IOError;
 import java.io.IOException;
 import java.io.StringReader;
 
 public class TryCatchResources {
     public static void main(String[] args) {
-        final BufferedReader br = new BufferedReader(new StringReader("Hello world!"));
-        try (BufferedReader reader = br) {
+        try (BufferedReader reader = new BufferedReader(new StringReader("Hello world!\nHello world2!"))) {
+            System.out.println(reader.readLine());
             System.out.println(reader.readLine());
         } catch (IOException e) {
             System.out.println("Error happened");
